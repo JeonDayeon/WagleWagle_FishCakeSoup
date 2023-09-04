3a988184bdc9b9b5e2b685ca78cf40eaf73c18b1
vydbwls <vydbwls12@gmail.com>
2023-09-05 04:04:13 +0900

    TimerSliderScript

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider timerSlider;
+    public float gameTime;
+
+    private bool stopTimer;
+
+    void Start()
+    {
+        stopTimer = false;
+        timerSlider.maxValue = gameTime;
+        timerSlider.value = gameTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = gameTime - Time.time;
+
+        if (time <= 0)
+        {
+            stopTimer = true;
+        }
+        if (stopTimer == false)
+        {
+            timerSlider.value = time;
+        }
+    }
+}
